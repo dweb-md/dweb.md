89c81c52fc4ec081df6b658319042dd5108db03a
Nicu Vrabii <nicuvrabii1@gmail.com>
2022-09-20 12:33:50 +0200

    Reviewed copy and added illustrations

--- src/pages/LandingPage/sections/LandingSection/utils.tsx
+++ src/pages/LandingPage/sections/LandingSection/utils.tsx
@@ -7,23 +7,26 @@ function generateCardsConfig(localization: typeof copy.ro) {
   return [
     {
       caption: localization.hypothesis_one,
-      img: { src: '/img/power_to_the_peers.svg', alt: 'power to the peers' }
+      img: { src: '/img/landing_power_to_the_peers.svg', alt: 'power to the peers' }
     },
     {
       caption: localization.hypothesis_two,
-      img: { src: '/img/power_to_the_peers.svg', alt: 'power to the peers' }
+      img: { src: '/img/landing_comp_econ.svg', alt: 'native economic component' }
     },
     {
       caption: localization.hypothesis_three,
-      img: { src: '/img/power_to_the_peers.svg', alt: 'power to the peers' }
+      img: { src: '/img/landing_sec_n_privacy.svg', alt: 'security and privacy by default' }
     },
     {
       caption: localization.hypothesis_four,
-      img: { src: '/img/power_to_the_peers.svg', alt: 'power to the peers' }
+      img: {
+        src: '/img/landing_single_point_of_failure.svg',
+        alt: 'no more single point of failure'
+      }
     },
     {
       caption: localization.hypothesis_five,
-      img: { src: '/img/power_to_the_peers.svg', alt: 'power to the peers' }
+      img: { src: '/img/landing_transparency.svg', alt: 'transparent web services' }
     }
   ]
 }
