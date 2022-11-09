bdb2a33c07846f884a6fa62d07c92668a9a29d16
Nicu Vrabii <nicuvrabii1@gmail.com>
2022-11-09 14:01:39 +0100

    Removed telegram social link

--- src/pages/LandingPage/sections/SubscribeSection/SubscribeSection.tsx
+++ src/pages/LandingPage/sections/SubscribeSection/SubscribeSection.tsx
@@ -3,7 +3,6 @@ import { Icon } from '../../../../global/components'
 import { GlobalContext } from '../../../../global/contexts'
 import './subscribe_section.scss'
 
-const TELEGRAM_INVITE_LINK = 'https://t.me/+IA3xyp_C3tkyYjFk'
 const DISCORD_INVITE_LINK = 'https://discord.gg/7cCtefKy'
 
 function SubscribeSection() {
@@ -14,9 +13,6 @@ function SubscribeSection() {
       <div className="subscribe-section__title">{copy.dweb_is_in_development}</div>
       <div className="subscribe-section__subtitle">{copy.call_to_subscription}</div>
       <div className="subscribe-section__social">
-        <div className="social-link" onClick={() => window.open(TELEGRAM_INVITE_LINK)}>
-          <Icon variant="telegram" />
-        </div>
         <div className="social-link" onClick={() => window.open(DISCORD_INVITE_LINK)}>
           <Icon variant="discord" />
         </div>
